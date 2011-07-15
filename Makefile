8a36013e4d0e35f022d16bee1389ebaf69a865a5
Christian Jonassen <flyrev@gmail.com>
2011-07-15 19:29:16 +0200

    main updated

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
 C_FLAGS := -O2
 
-
-
 all:	main.o
-	g++ -lglut -lGL -L/usr/X11R6/lib -o Solskogen obj/*.o
+	g++ -lglut -lGL -lGLEW -L/usr/X11R6/lib -o Solskogen obj/*.o
 main.o:	main.cpp
 	g++ $(C_FLAGS) -c -o obj/main.o main.cpp
 clean:
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <cstdlib>
-//#include <GL/glew.h>
+#include <GL/glew.h>
 #include <GL/glut.h>
 #include <cmath>
 #include <iostream>
@@ -7,25 +7,43 @@
 
 using namespace std;
 
+void update()
+{
+	
+	glutPostRedisplay();
+}
+
+void render()
+{
+	glClearColor(0.0f,0.0f,0.0f,1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 	int t = glutCreateWindow( "Curveball" );
 
-	glutReshapeWindow( 800, 800 );
-
-
-
+	glutInitWindowSize( 800, 800 );
 
 	cout << "Spill" << endl;
 	cout << "It's a game" << endl;
 
 	cout << "Loading ..." << endl;
 	
+	glewInit();
+
 	srand(time(NULL));
 	int random=rand();
   	cout << "You have " << random << " points" << endl;
+	
+	glutIdleFunc(&update);
+	glutDisplayFunc(&render);
 	glutMainLoop();	
+
+	
 	return EXIT_SUCCESS;
 }
 

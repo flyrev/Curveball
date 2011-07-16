b2d05f2d6ff37235102ff86037346e4658d49861
Christian Jonassen <flyrev@gmail.com>
2011-07-16 14:49:37 +0200

    Ball > vannmelon

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 
 #define PI 3.14
 
+int frames=0;
+
 using namespace std;
 
 Quad *quad;
@@ -22,6 +24,8 @@ GLVertexAttribute *quadposition;
 GLVertexAttribute *ballposition;
 GLVertexIndices *indices;
 Sphere *sphere;
+
+Uniform *rotation;
 
 void keyboard(unsigned char key, int x, int y)
 {
@@ -41,12 +45,17 @@ void update()
 void render()
 {
 	glClearColor(0.0f,0.0f,0.1f,1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glEnable(GL_BLEND);
-
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_FRONT);
+	//	glEnable(GL_BLEND);
+	
+	rotation->Matrix(mat4::Rotation(vec3(0,1,0), frames++/1999.0f));
+	
 	sphereShader->Use();
 	ballposition->Enable();
-	glDrawElements(GL_QUADS, sphere->getIndexCount(), GL_UNSIGNED_SHORT, sphere->getIndexData());
+	glDrawElements(GL_TRIANGLES, sphere->getIndexCount(), GL_UNSIGNED_SHORT, sphere->getIndexData());
 	ballposition->Disable();
 	
 	quadShader->Use();
@@ -83,7 +92,8 @@ int main(int argc, char **argv)
 	quadposition = new GLVertexAttribute("position", 2, 0, 0, quadShader, 4, quad->getVertexData());
 
 	sphereShader = new GLShaderProgram("sphere");
-	sphere = new Sphere(0.25,60,60);
+	sphere = new Sphere(0.25,100,100);
+	rotation = sphereShader->getUniform("rotation");
 	ballposition = new GLVertexAttribute("position", 3, 0, 0, sphereShader, sphere->getVertexCount(), sphere->getVertexData());
 
 	glutMainLoop();	
--- sphere.vert
+++ sphere.vert
@@ -1,9 +1,10 @@
 attribute vec3 position;
 varying vec3 normal;
+uniform mat4 rotation;
  
 void main()
 {
-	gl_Position = vec4(position,1.0);
+	gl_Position = rotation*vec4(position,1.0);
 	gl_Position.xy*=2;
 	normal = position;
 }
